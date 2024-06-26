34f032f64ab4a4ac058516c35a40173c177e1be0
VicTech444 <Victor_verq@outlook.com>
2024-06-26 09:45:38 -0500

    possible fix for settings page

--- src/app/settings/page.tsx
+++ src/app/settings/page.tsx
@@ -16,14 +16,15 @@ interface Props {
 
 export default function Settings() {
   const userInformation = useHandlePersonalInfo();
-  
+
+  if (userInformation.data) console.log(userInformation.data.message[0]);
   return (
     <div>
       <Navbar />
       <Header />
       <div className="container flex flex-col gap-y-4">
-        {!userInformation ? (
-          <h1>No content available</h1>
+        {!userInformation.data ? (
+          <FaSpinner className="h-6 w-6 animate-spin" />
         ) : userInformation.isLoading ? (
           <FaSpinner className="h-6 w-6 animate-spin" />
         ) : (
