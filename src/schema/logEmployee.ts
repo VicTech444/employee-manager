a7dcc5faabc20dbb0dfd5bcd07fdf45b296a3fee
VicTech444 <Victor_verq@outlook.com>
2024-06-25 20:24:39 -0500

    add interface to login schema

--- src/schema/logEmployee.ts
+++ src/schema/logEmployee.ts
@@ -1,11 +1,16 @@
 import z from 'zod'
 
+interface Props {
+    email: string;
+    password: string;
+}
+
 const logEmployeeSchema = z.object({
     email: z.string().min(10, {message: "Email length has to be 10"}).email({message: "Invalid email address"}),
     password: z.string().min(8, {message: "Password length has to be 8"})
 });
 
-export const validateEmployeeCredentials = async (credentials) => {
+export const validateEmployeeCredentials = async (credentials : Props) => {
     return logEmployeeSchema.safeParse(credentials);
 }
 
